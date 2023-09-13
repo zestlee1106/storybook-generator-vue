f9549e4ac216d94555e8a1027651583e051fac5a
이영우 <zestlee1106@gmail.com>
2023-09-13 15:12:40 +0900

    일단 수정중

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,58 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "createStory",
+    async (resource: vscode.Uri) => {
+      if (resource) {
+        // 현재 열려있는 프로젝트의 루트 경로 가져오기
+        const projectRoot = vscode.workspace.workspaceFolders?.[0].uri.fsPath;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-extension-vue" is now active!');
+        if (!projectRoot) {
+          vscode.window.showErrorMessage("프로젝트가 열려있지 않습니다.");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('test-extension-vue.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test-extension-vue!');
-	});
+          return;
+        }
 
-	context.subscriptions.push(disposable);
+        // 선택한 폴더 경로 가져오기
+        const folderPath = resource.fsPath;
+
+        // 파일 이름만 가져오기
+        const fileNameWithExtension = path.basename(folderPath);
+
+        // 파일 정보 분석
+        const fileInfo = path.parse(fileNameWithExtension);
+
+        // 확장자를 제외한 파일 이름 가져오기
+        const fileNameWithoutExtension = fileInfo.name;
+
+        const filePath = path.join(projectRoot, fileNameWithoutExtension);
+
+        vscode.window.showInformationMessage(filePath);
+
+        if (!fs.existsSync(filePath)) {
+          fs.mkdirSync(filePath);
+
+          const storybookPath = path.join(
+            filePath,
+            `${fileNameWithoutExtension}.stories.tsx`
+          );
+
+          fs.writeFileSync(storybookPath, `import * from 'fs'`);
+
+          vscode.window.showInformationMessage("생성 완료");
+        }
+
+        vscode.window.showInformationMessage("이미 있음");
+
+        vscode.window.showInformationMessage("하이욤");
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
